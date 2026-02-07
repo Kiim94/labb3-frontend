8f4d300252ebc74db0bb8da512f35c5a1b453227
Kiim94 <nemomoa@hotmail.com>
2026-02-07 11:59:53 +0100

    Första commait: gjort färgschema, några mixins, color functions

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    //skriv vilket plugin som används
+    plugins: [react(),
+        ViteImageOptimizer({
+            png: {quality:80},
+            jpeg:{quality:90},
+            webp:{quality: 70},
+            avif:{quality:80},
+            svg: {
+                plugins:[
+                    {name:"removeViewBox", active:false},
+                    {name: "sortAttrs"},
+                ]
+            }
+        })
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                sass: resolve(__dirname, "sass.html"),
+            }
+        }
+    }
+});
